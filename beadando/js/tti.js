288f1d4e48998b934a40131c6029dc499b0db005
NYuNYork <95426913+NYuNYork@users.noreply.github.com>
2023-01-31 23:43:40 +0100

    Add files via upload

---
+++ beadando/js/tti.js
@@ -0,0 +1,8 @@
+function TTI()
+{
+    let kg = document.getElementById("kg").value;
+    let cm = document.getElementById("cm").value;
+    let magassag = cm/100;
+    let eredmeny = suly / (magassag*magassag)
+    document.getElementById("eredmeny").innerHTML;
+}
